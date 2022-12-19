39a1d7afa39b13f2349bd7bf9857371c07a8b4ed
Gerardo Parra <parra.gerardo@gmail.com>
2022-12-19 16:31:13 +0100

    Uso de HostLIstener para manejo de eventos mediante atributos

--- src/app/directivas/destacado/destacado.directive.ts
+++ src/app/directivas/destacado/destacado.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[destacado]'
 })
 export class DestacadoDirective {
 
-  constructor(private el: ElementRef) {
+  @HostListener('mouseenter') onMouseEnter() {
     this.el.nativeElement.style.backgroundColor = 'yellow';
   }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.el.nativeElement.style.backgroundColor = '';
+  }
+
+  constructor(private el: ElementRef) {}
+
 
 }
