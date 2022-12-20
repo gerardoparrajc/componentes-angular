d027e5e30d9040b6693fb62412279ff1cb7c2206
Gerardo Parra <parra.gerardo@gmail.com>
2022-12-20 16:20:56 +0100

    Implementadas peticiones GET, POST, PUT y DELETE con HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,9 @@ import { MiServicioService } from './services/mi-servicio.service';
 })
 export class AppComponent {
   constructor(private servicio: MiServicioService) {
-    console.log(servicio.getPosts());
+    console.log(servicio.getPost(548));
+    console.log(servicio.createPost('hola mundo', 'cuerpo del hola mundo', 4));
+    console.log(servicio.updatePost(3, 'hola mundo', 'cuerpo del hola mundo', 4));
+    console.log(servicio.deletePost(83));
   }
 }
--- src/app/services/mi-servicio.service.ts
+++ src/app/services/mi-servicio.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 export class MiServicioService {
 
   public numero: number = 0;
-  private urlServer: string = 'https://jsonplaceholder.typicode.com3/';
+  private urlServer: string = 'https://jsonplaceholder.typicode.com/';
 
   constructor(private http: HttpClient) {
     this.numero = Math.random() * 10;
@@ -23,6 +23,48 @@ export class MiServicioService {
 
   public getPosts() {
     this.http.get(this.urlServer + 'posts').subscribe({
+      next: (result) => console.log(result),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completado')
+    });
+  }
+
+  public getPost(id: number) {
+    this.http.get(this.urlServer + 'posts/' + id).subscribe({
+      next: (result) => console.log(result),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completado')
+    });
+  }
+
+  public createPost(titulo: string, cuerpo: string, idUsuario: number) {
+    const params = JSON.stringify({
+      title: titulo,
+      body: cuerpo,
+      userId: idUsuario
+    });
+    this.http.post(`${this.urlServer}posts`, params).subscribe({
+      next: (result) => console.log(result),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completado')
+    });
+  }
+
+  public updatePost(id: number, titulo: string, cuerpo: string, idUsuario: number) {
+    const params = JSON.stringify({
+      title: titulo,
+      body: cuerpo,
+      userId: idUsuario
+    });
+    this.http.put(`${this.urlServer}posts/${id}`, params).subscribe({
+      next: (result) => console.log(result),
+      error: (error) => console.log(error),
+      complete: () => console.log('Completado')
+    });
+  }
+
+  public deletePost(id: number) {
+    this.http.delete(`${this.urlServer}posts/${id}`).subscribe({
       next: (result) => console.log(result),
       error: (error) => console.log(error),
       complete: () => console.log('Completado')
