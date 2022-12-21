e0738830809d09dbf26e1a2bc732be93bdd93b34
Gerardo Parra <parra.gerardo@gmail.com>
2022-12-21 16:42:57 +0100

    Implementado el cambio de ruta en enlaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { BotonesModule } from './botones/botones.module';
+// import { BotonesModule } from './botones/botones.module';
 import { RouterModule, Routes } from '@angular/router';
 import { Cuerpo1Component } from './cuerpo1/cuerpo1.component';
 import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
 import { Cuerpo3Component } from './cuerpo3/cuerpo3.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { BotonesComponent } from './botones/botones.component';
+import { BotonComponent } from './boton/boton.component';
+import { DestacadoDirective } from './directivas/destacado/destacado.directive';
 
 
 const rutas:Routes = [
@@ -23,10 +26,13 @@ const rutas:Routes = [
   declarations: [
     AppComponent,
     PageNotFoundComponent,
+    BotonesComponent,
+    BotonComponent,
+    DestacadoDirective
   ],
   imports: [
     BrowserModule,
-    BotonesModule,
+    // BotonesModule,
     HttpClientModule,
     RouterModule.forRoot(rutas)
   ],
