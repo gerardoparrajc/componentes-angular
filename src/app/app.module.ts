89ecbcc78d781222f3ba97ceeef3f1d3c93e7256
Gerardo Parra <parra.gerardo@gmail.com>
2022-12-21 16:26:54 +0100

    Añadidas rutas por defecto y de error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { Cuerpo1Component } from './cuerpo1/cuerpo1.component';
 import { Cuerpo2Component } from './cuerpo2/cuerpo2.component';
 import { Cuerpo3Component } from './cuerpo3/cuerpo3.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const rutas:Routes = [
   { path: 'boton/1', component: Cuerpo1Component },
   { path: 'boton/2', component: Cuerpo2Component },
   { path: 'boton/3', component: Cuerpo3Component },
+  { path: '', redirectTo: '/boton/1', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
