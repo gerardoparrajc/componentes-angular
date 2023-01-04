5f74e326fb0fad4584de744b4d8a91ea2aa06255
Gerardo Parra <parra.gerardo@gmail.com>
2023-01-04 12:29:53 +0100

    Formulario inicial

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -8,41 +8,6 @@ import { NgForm } from '@angular/forms';
 })
 export class FormularioComponent {
 
-  @ViewChild('formulario') formulario?: NgForm;
-  defaultCity = 'almeria';
-  defaultUsername = 'test';
-  username = '';
-  usuario = {
-    nombreUsuario: '',
-    correoElectronico: '',
-    ciudad: ''
-  };
 
-
-  onSubmit() {
-    this.usuario.nombreUsuario = this.formulario?.value.datosUsuario.username;
-    this.usuario.correoElectronico = this.formulario?.value.datosUsuario.email;
-    this.usuario.ciudad = this.formulario?.value.city;
-
-    this.formulario?.reset();
-
-    console.log(this.usuario);
-  }
-
-  rellenarUsuario() {
-    /* this.formulario?.setValue({
-      datosUsuario: {
-        username: 'test',
-        email: 'test@test.com'
-      },
-      city: 'madrid'
-    }); */
-
-    this.formulario?.form.patchValue({
-      datosUsuario: {
-        username: 'test'
-      }
-    });
-  }
 
 }
